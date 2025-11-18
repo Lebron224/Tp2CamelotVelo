b9889812cdfef9126eb61af614ff9ba4e75e8dc1
Lebron224 <zachlegros25@gmail.com>
2025-11-18 16:34:59 -0500

    Il reste juste a faire la classe Niveau pour completer logique  (Rajouter classe Maison)

--- src/main/java/ca/qc/bdeb/sim/tp2camelotvelo/Niveau.java
+++ src/main/java/ca/qc/bdeb/sim/tp2camelotvelo/Niveau.java
@@ -3,5 +3,7 @@ package ca.qc.bdeb.sim.tp2camelotvelo;
 import java.util.ArrayList;
 
 public class Niveau {
-
+    private ArrayList<Maison> maisons;
+    private Camelot camelot;
+    private ArrayList<Journaux> journaux;
 }
